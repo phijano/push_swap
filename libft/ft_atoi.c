b23f69ac8fdb3efd69cae75896fdde9d8f73a0b8
Pablo alberto Hijano rueda <you@example.com>
2022-09-05 10:14:57 +0200

    github first

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: phijano- <phijano-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:22:04 by phijano-          #+#    #+#             */
-/*   Updated: 2022/06/28 13:17:49 by phijano-         ###   ########.fr       */
+/*   Updated: 2022/07/07 14:36:32 by phijano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,17 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	count = 0;
 	num = 0;
+	while (str[count] == ' ' || str[count] == '\t' || str[count] == '\f'
+		|| str[count] == '\r' || str[count] == '\n' || str[count] == '\v')
+		count++;
 	if (str[count] == '+' || str[count] == '-')
 		if (str[count++] == '-')
 			sign = -1;
 	while (str[count] >= '0' && str[count] <= '9')
-	{
-		num = num * 10 + (str[count] - '0');
-		count++;
-	}
+		num = num * 10 + (str[count++] - '0');
 	if (str[count])
 		num = 0;
-	if (sign > 0)
-		if (num > 2147483647)
-			return (0);
-	if (sign < 0)
-		if (num > 2147483648)
-			return (0);
+	if ((sign > 0 && num > 2147483647) || (sign < 0 && num > 2147483648))
+		return (0);
 	return ((int)(num * sign));
 }
